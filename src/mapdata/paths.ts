51d292da73035c6417bbd1bfb8c0e386fc89e44b
toBeOfUse <mitchjacov@gmail.com>
2021-07-30 21:23:47 -0400

    add railroad path

--- src/mapdata/paths.ts
+++ src/mapdata/paths.ts
@@ -6,11 +6,14 @@ const paths: PathData[] = [
         { x: 420, y: 250 },
         { x: 420, y: 508 },
         { x: 788, y: 508 }
+    ]),
+    new PathData("/minecart.png", [
+        { x: -43, y: -57 },
+        { x: -451, y: -57 },
+        { x: -451, y: -193 },
+        { x: -1195, y: -919 },
+        { x: -1366, y: -919 }
     ])
 ];
 
 export default paths;
-
-/*
-
-*/
