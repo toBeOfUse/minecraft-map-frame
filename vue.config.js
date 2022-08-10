8671964cedab380b8bacf276cb6bc6ac846c453b
toBeOfUse <mitchjacov@gmail.com>
2022-08-09 20:11:29 -0400

    place in directory /minecraft/

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,19 @@
 const path = require("path");
-const CircularDependencyPlugin = require('circular-dependency-plugin');
+const CircularDependencyPlugin = require("circular-dependency-plugin");
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
-    outputDir: process.env.OUTPUT_DIR || path.resolve(__dirname, "./dist"),
-    configureWebpack: {
-        plugins: [
-            new CircularDependencyPlugin(),
-            // new BundleAnalyzerPlugin()
-        ]
-    },
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].title = 'everything the light touches'
-                return args
-            })
-    },
-}
+  outputDir: process.env.OUTPUT_DIR || path.resolve(__dirname, "./dist"),
+  publicPath: "/minecraft/",
+  configureWebpack: {
+    plugins: [
+      new CircularDependencyPlugin()
+      // new BundleAnalyzerPlugin()
+    ]
+  },
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "everything the light touches";
+      return args;
+    });
+  }
+};
